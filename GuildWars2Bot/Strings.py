8e782d5792d169931fb7f56911fd23f19b270373
DartDivora <nsanft@sanft.com>
2017-07-05 10:54:51 -0500

    Moved GuildWars2Bot code to another repo
    Moved all GuildWars2Bot code and removed references to it in the
    .gitignore. This is being moved into its own repo.

--- GuildWars2Bot/Strings.py
+++
@@ -1,8 +0,0 @@
-all = {
-    "exchange" : ("Here are how many {0} you could get for {1} {2}: \nCoins per gem: {3}\nAmount: {4} {5}"),
-    "count_with_total" : ("You have {0} {1}(s) out of a possible total of {2} unlocked on your account."),
-    "too_many_items": ("Too many results (got {0}, max is {1})! Please refine your search."),
-    "no_results": ("No results! Please refine your search."),
-    "no_api_key": ("API Key Not Registered!"),
-    "list": ("Here is a list of {0}, maybe ANet will make more one day...")
-}
